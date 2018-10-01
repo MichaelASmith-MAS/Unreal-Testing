f628775c5a7b24be921dfcfb4142b08febda5daf
Michael Smith <31556510+MichaelASmith-MAS@users.noreply.github.com>
2018-10-01 12:45:12 -0700

    Open Door Prototype

---
+++ UnrealTesting/Source/UnrealTesting/OpenDoor.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+#include "Engine/World.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+	bIsOpen = false;
+
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	OpenDoor();
+	CloseDoor();
+}
+
+
+void UOpenDoor::OpenDoor()
+{
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+
+		GetOwner()->SetActorRotation(FRotator(0.f, OpenAngle, 0.f));
+
+	}
+}
+
+void UOpenDoor::CloseDoor()
+{
+	if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime >= TimeToClose)
+	{
+		GetOwner()->SetActorRotation(FRotator(0.f, 0.f, 0.f));
+	}
+	
+}
---
+++ UnrealTesting/Source/UnrealTesting/OpenDoor.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "GameFramework/Actor.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UNREALTESTING_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void OpenDoor();
+	void CloseDoor();
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+		AActor* ActorThatOpens;
+
+	UPROPERTY(EditAnywhere)
+		float OpenAngle = 60.f;
+
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate;
+	
+	UPROPERTY(VisibleAnywhere)
+		bool bIsOpen;
+
+	UPROPERTY(EditAnywhere)
+		float TimeToClose = 1.f;
+
+	UPROPERTY(VisibleAnywhere)
+		float LastDoorOpenTime = 0.f;
+
+};
